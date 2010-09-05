eef16106e49d8bf5e0e0476004d878c98a3a7f47
Gabriel Preston <gpreston@gmail.com>
2010-09-05 00:19:03 -0500

    Built schema for initial database

---
+++ config/schema/schema.php
@@ -0,0 +1,15 @@
+<?php 
+/* SVN FILE: $Id$ */
+/* Contacts schema generated on: 2010-09-05 00:09:38 : 1283663918*/
+class ContactsSchema extends CakeSchema {
+	var $name = 'Contacts';
+
+	function before($event = array()) {
+		return true;
+	}
+
+	function after($event = array()) {
+	}
+
+}
+?>
