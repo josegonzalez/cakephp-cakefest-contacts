86798340cb1929d95758a0b264b58cc6bcc9e3e5
Jose Diaz-Gonzalez <myaccounts@savant.be>
2010-09-05 00:19:07 -0500

    capistrano deploy scripts

---
+++ config/deploy/dev.rb
@@ -0,0 +1,3 @@
+role :web, "devcontacts.josediazgonzalez.com"
+set :branch, 'master'
+set :deploy_to, "/home/jose/public_html/josediazgonzalez.com/devcontacts"
---
+++ config/deploy/prod.rb
@@ -0,0 +1,3 @@
+role :web, "contacts.josediazgonzalez.com"
+set :branch, 'master'
+set :deploy_to, "/home/jose/public_html/josediazgonzalez.com/contacts"
